39d9d2ac97c1bc03b7cfde8c8373586ad812f660
fli666-oss <feng.li@hotmail.fr>
2026-04-11 20:57:52 +0200

    update service conf

--- flight_project/install_service.bat
+++ flight_project/install_service.bat
@@ -1,11 +1,13 @@
 @echo off
+setlocal EnableDelayedExpansion
 echo ========================================
 echo   NSSM Windows Service Installer
 echo ========================================
 echo.
 
 set "serviceName=FlightSearchServer"
-set "batPath=%~dp0start.bat"
+set "projectPath=%~dp0flight_project"
+set "pythonPath=C:\Users\fengl\AppData\Local\Programs\Python\Python313\python.exe"
 
 if not exist "nssm.exe" (
     echo Downloading NSSM...
@@ -16,11 +18,19 @@ if not exist "nssm.exe" (
     del nssm.zip >nul 2>&1
 )
 
+echo Installing dependencies...
+"%pythonPath%" -m pip install flask flask-sqlalchemy flask-swagger-ui serpapi -q
+
+echo Uninstalling existing service if any...
+nssm.exe remove "%serviceName%" confirm >nul 2>&1
+
 echo Installing service: %serviceName%...
-nssm.exe install "%serviceName%" "%batPath%"
-nssm.exe set "%serviceName%" AppDirectory "%~dp0flight_project"
+nssm.exe install "%serviceName%" "%pythonPath%"
+nssm.exe set "%serviceName%" AppDirectory "%projectPath%"
+nssm.exe set "%serviceName%" AppParameters "-c \"from run import app; app.run(host='0.0.0.0', port=5000)\""
 nssm.exe set "%serviceName%" DisplayName "Flight Search Server"
 nssm.exe set "%serviceName%" Description "Flight search Flask server"
+nssm.exe set "%serviceName%" AppEnvironmentExtra "PATH=C:\Users\fengl\AppData\Local\Programs\Python\Python313;C:\Users\fengl\AppData\Local\Programs\Python\Python313\Scripts;%PATH%&SERPAPI_API_KEY=3a9aaedb965e62e9bbc50abf516f4bfbf1b84f3c3eb41db3b7b1062336f28d28&USE_SERPAPI=true"
 
 echo.
 echo Starting service...
